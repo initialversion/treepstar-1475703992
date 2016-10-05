ee77115054dccbc7eb9a3cc9da363baa89658940
Raghu Betina <raghu@firstdraft.com>
2016-10-05 21:48:53 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "evaluations#index"
   # Routes for the Evaluation resource:
   # CREATE
