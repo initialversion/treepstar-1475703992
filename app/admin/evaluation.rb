ae414533cd73e905ee5d88dd9a363c31d2429685
Raghu Betina <raghu@firstdraft.com>
2016-10-05 21:49:01 +0000

    Whitelist columns in Evaluation for ActiveAdmin

--- app/admin/evaluation.rb
+++ app/admin/evaluation.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Evaluation do
 
+ permit_params :reviewer_id, :reviewee_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
