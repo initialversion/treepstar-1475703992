72b677aec1a00bfbcf3a2ec979e1be2428ea5561
Raghu Betina <raghu@firstdraft.com>
2016-10-05 21:48:39 +0000

    Direct association between users and evaluations

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -1,6 +1,9 @@
 class Evaluation < ApplicationRecord
   # Direct associations
 
+  belongs_to :reviewee,
+             :class_name => "User"
+
   belongs_to :reviewer,
              :class_name => "User"
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :received_evaluations,
+             :class_name => "Evaluation",
+             :foreign_key => "reviewee_id",
+             :dependent => :destroy
+
   has_many   :evaluations,
              :foreign_key => "reviewer_id",
              :dependent => :destroy
